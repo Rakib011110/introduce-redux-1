dee80fe9748a9524010aa84c79a9d71f50bb1255
Rakibul Islam <rakib011110@gmail.com>
2023-09-02 22:50:43 +0600

    redux slice add

---
+++ src/Components/Pages/Counter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div className="bg-purple-500 p-7 font-bold rounded-ee-3xl border-e-2 border-e-red-400">
+      <button> Increament</button>
+      <h1>0 </h1>
+      <button> decreament</button>
+    </div>
+  );
+};
+
+export default Counter;
---
+++ src/Components/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Counter from "./Counter";
+
+const Home = () => {
+  return (
+    <div>
+      <Counter></Counter>
+    </div>
+  );
+};
+
+export default Home;
